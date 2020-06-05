ef64306bf3c8d28fe26e1c10818fb265fc4ba839
Mariya <marikor168@gmail.com>
2020-06-05 18:30:53 +0300

    https

--- src/services/emphaService.js
+++ src/services/emphaService.js
@@ -1,5 +1,5 @@
 export function fetchToken() {
-  return fetch("http://emphasoft-test-assignment.herokuapp.com/api-token-auth/", {
+  return fetch("https://emphasoft-test-assignment.herokuapp.com/api-token-auth/", {
     method: "POST",
     body: JSON.stringify({username: "test_super", password: "Nf<U4f<rDbtDxAPn"}),
     headers: {
@@ -10,7 +10,7 @@ export function fetchToken() {
 }
 
 export function fetchUsers(token) {
-  return fetch("http://emphasoft-test-assignment.herokuapp.com/api/v1/users/", {
+  return fetch("https://emphasoft-test-assignment.herokuapp.com/api/v1/users/", {
     method: "GET",
     headers: {
           'Authorization': `Token ${token}`,
